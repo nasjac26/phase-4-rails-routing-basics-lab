15a1995319980d83080c13858638ff9efd4c23a6
Jacob <Jacobonasrallah@gmail.com>
2021-11-01 10:39:57 -0700

    commit tree bb47a8341683e6c336ea4cd104457146b783be6d

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+  def index
+    render json: Student.all
+  end
+
+  def grades
+    render json: Student.order("grade DESC")
+  end
+
+  def highest_grade
+    render json: Student.order("grade DESC").first
+    
+  end
+end
